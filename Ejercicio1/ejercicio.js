469d3d9d848ec788147d55e18d280e897d75e44e
TioOti <octavio.ripoll25@hotmail.com>
2022-12-09 13:49:10 -0300

    Ejercicio 1 - OBC

---
+++ Ejercicio1/ejercicio.js
@@ -0,0 +1,14 @@
+const nombre = "Octavio";
+const edad = 28;
+const developer = true;
+const birthDate = new Date("September 22 1994");
+const favoriteBook = {
+  titulo: "Mi planta de Naranja Lima",
+  autor: " José Mauro de Vasconcelos",
+  fecha: new Date(1802),
+  url: "https://es.wikipedia.org/wiki/Mi_planta_de_naranja_lima",
+};
+
+const personalDescription = [nombre, edad, developer, birthDate, favoriteBook];
+
+console.log(personalDescription)
